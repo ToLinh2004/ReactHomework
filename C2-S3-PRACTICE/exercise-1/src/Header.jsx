e8e593182ae0056f024499e6e40dedce7cf3fc92
Linh <nguyenthilinhqni2020@gmail.com>
2024-05-11 16:57:33 +0700

    create component

---
+++ C2-S3-PRACTICE/exercise-1/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="block">
+      <h2>WELCOME TO THIS COURSE!</h2>
+    </header>
+  );
+}
+
+export default Header;
---
+++ C2-S3-PRACTICE/exercise-2/exercise2/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="block">
+      <h1>WELCOME !</h1>
+    </header>
+  );
+}
+
+export default Header;
